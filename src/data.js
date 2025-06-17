0cd0c35c517e4600feb2bbe883fb2fba9a040840
HiepDH <hiepdh@hmgames.com>
2025-06-17 17:27:03 +0700

    u

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const BestPA = [
       // TC3_Playable_V1_01
       // MAC_Playable_V1_01_GamePlay
       // MAC_Playable_V5_01_Ref1
-         {
+      {
         path: "/PATestDemo/MAC_Playable_V6_06_Airport_FullStickers.html",
         name: "MAC_Playable_V6_06_Airport_FullStickers",
         dev: "",
