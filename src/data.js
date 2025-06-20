858b1bf0747c80a06aae5fc47f6a654a44971f59
HiepDH <hiepdh@hmgames.com>
2025-06-20 18:15:42 +0700

    u

--- src/data.js
+++ src/data.js
@@ -5,15 +5,14 @@ const BestPA = [
       // TC3_Playable_V1_01
       // MAC_Playable_V1_01_GamePlay
       // MAC_Playable_V5_01_Ref1
-
    {
-        path: "/PATestDemo/MJAD_Playable_V2_02_Ref1.html",
-        name: "MJAD_Playable_V2_02_Ref1",
+        path: "/PATestDemo/MJAD_Playable_V2_01_Ref1.html",
+        name: "MJAD_Playable_V2_01_Ref1",
         dev: "",
       },
      {
-        path: "/PATestDemo/MJAD_Playable_V2_01_Ref3.html",
-        name: "MJAD_Playable_V2_01_Ref3",
+        path: "/PATestDemo/MJAD_Playable_V2_02_Ref2.html",
+        name: "MJAD_Playable_V2_02_Ref2",
         dev: "",
       },
       {
