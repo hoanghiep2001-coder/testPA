2324653b6a7a2965c76689ac06b0270517a39c68
HiepDH <hiepdh@hmgames.com>
2025-06-26 22:33:19 +0700

    u

--- src/data.js
+++ src/data.js
@@ -591,62 +591,6 @@ const BestPA = [
       // },
     ],
   },
-
-  // {
-  //     timeline: "Tháng 7:",
-  //     data: [
-  //         {
-  //             path: "/BestPA/Month7_2024/BattleDoll_Playable_MTG_Op1_GP_Click_PkMode.html",
-  //             name: "BattleDoll_Playable_MTG_Op1_GP_Click_PkMode",
-  //             dev: "Quân",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable.html",
-  //             name: "Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable",
-  //             dev: "Phong",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker.html",
-  //             name: "Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker",
-  //             dev: "Phong",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow.html",
-  //             name: "Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow",
-  //             dev: "Phong",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_1_GP_Level12.html",
-  //             name: "Satisday_Playable_Applovin_Op3_1_GP_Level12",
-  //             dev: "Hiệp",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_GP_Level12.html",
-  //             name: "Satisday_Playable_Applovin_Op3_GP_Level12",
-  //             dev: "Hiệp",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon.html",
-  //             name: "StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon",
-  //             dev: "Hiệp",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku.html",
-  //             name: "StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku",
-  //             dev: "Hiệp",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP4_Drag_GP_Fight.html",
-  //             name: "StickCraft_Playable_Applovin_OP4_Drag_GP_Fight",
-  //             dev: "Quân",
-  //         },
-  //         {
-  //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP5_Click_FGP_Fight.html",
-  //             name: "StickCraft_Playable_Applovin_OP5_Click_FGP_Fight",
-  //             dev: "Quân",
-  //         },
-  //     ]
-  // },
 ];
 
 export default BestPA;
