51302c4e2c637b2c0ec28a137e7af813a52c3a75
HiepDH <hiepdh@ieccorp.vn>
2025-07-21 11:27:51 +0700

    u

--- src/data.js
+++ src/data.js
@@ -3,11 +3,16 @@ const BestPA = [
     timeline: "List:",
     data: [
       {
+        path: "/PATestDemo/RC2_V8_Version1_NoBorderBackground.html",
+        name: "RC2_V8_Version1_NoBorderBackground",
+        dev: "",
+      },
+      {
         path: "/PATestDemo/RC2_V8_Version2.html",
         name: "RC2_V8_Version2",
         dev: "",
       },
-        {
+      {
         path: "/PATestDemo/RC2_V8_Version1.html",
         name: "RC2_V8_Version2",
         dev: "",
