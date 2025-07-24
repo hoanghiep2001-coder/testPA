aef64b33b9b6e6aa800a1f7017ab012256f2700b
HiepDH <hiepdh@hmgames.com>
2025-07-24 11:25:37 +0700

    u

--- src/data.js
+++ src/data.js
@@ -12,6 +12,26 @@ const BestPA = [
         name: "RC2_V9_Version2_FullborderBacgrkound",
         dev: "",
       },
+         {
+        path: "/PATestDemo/SR_Playable_V2_02_Renamed_Unity_IOS.html",
+        name: "SR_Playable_V2_02_Renamed_Unity_IOS",
+        dev: "",
+      },
+         {
+        path: "/PATestDemo/SR_Playable_V2_01_Renamed_encrypted_ALV.html",
+        name: "SR_Playable_V2_01_Renamed_encrypted_ALV",
+        dev: "",
+      },
+         {
+        path: "/PATestDemo/SR_Playable_V2_01_Renamed_Unity_IOS.html",
+        name: "SR_Playable_V2_01_Renamed_Unity_IOS",
+        dev: "",
+      },
+         {
+        path: "/PATestDemo/SR_Playable_V2_02_Renamed_encrypted_ALV.html",
+        name: "SR_Playable_V2_02_Renamed_encrypted_ALV",
+        dev: "",
+      },
     ],
   },
 ];
