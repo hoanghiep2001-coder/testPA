8f1915a6e65c75352e34c8e76f2ca6779b88942d
HiepDH <hiepdh@hmgames.com>
2025-12-12 10:42:09 +0700

    u

--- src/data.js
+++ src/data.js
@@ -2,43 +2,57 @@ const BestPA = [
   {
     timeline: "List:",
     data: [
-      
          {
+        path: "/PATestDemo/HJP_Playable_V16_01_RmkBestPLATheme00_RemakePlayableV4_02_Limit90s_Mul2x_applovin.html",
+        name: "HJP_Playable_V16_01_RmkBestPLATheme00_RemakePlayableV4_02_Limit90s_Mul2x_applovin",
+        dev: "",
+      },
+         {
+        path: "/PATestDemo/HJP_Playable_V16_02_RemakeBestPlayableTheme00_PlayableV5_05_Limit90s_applovin.html",
+        name: "HJP_Playable_V16_02_RemakeBestPlayableTheme00_PlayableV5_05_Limit90s_applovin",
+        dev: "",
+      },
+         {
+        path: "/PATestDemo/HJP_Playable_V16_03_RemakeBestPlayableTheme00_RemakePlayableV1_01_Limit5Moves_applovin.html",
+        name: "HJP_Playable_V16_03_RemakeBestPlayableTheme00_RemakePlayableV1_01_Limit5Moves_applovin",
+        dev: "",
+      },
+      {
         path: "/PATestDemo/SantaCall_Playable_V1_01_FakeVideoCall_SantaClaus_unity.html",
         name: "SantaCall_Playable_V1_01_FakeVideoCall_SantaClaus_unity",
         dev: "",
-      },   
+      },
       {
         path: "/PATestDemo/PJP_Playable_V4_VisualInGame_Test.html",
         name: "PJP_Playable_V4_VisualInGame_Test",
         dev: "",
       },
-          {
+      {
         path: "/PATestDemo/SantaCall_Playable_Applovin_Op1_GP_Click_Call.html",
         name: "SantaCall_Playable_Applovin_Op1_GP_Click_Call",
         dev: "",
       },
-          {
+      {
         path: "/PATestDemo/SantaCall_Playable_Applovin_Op2_GP_Click_VideoCall.html",
         name: "SantaCall_Playable_Applovin_Op2_GP_Click_VideoCall",
         dev: "",
       },
-       {
+      {
         path: "/PATestDemo/RC_V15.html",
         name: "RC_V15",
         dev: "",
       },
-        {
+      {
         path: "/PATestDemo/RC_V16.html",
         name: "RC_V16",
         dev: "",
       },
-         {
+      {
         path: "/PATestDemo/RC2_V13Layout_2.html",
         name: "RC2_V13Layout_2",
         dev: "",
       },
-       {
+      {
         path: "/PATestDemo/RC2_Playable_V10.html",
         name: "RC2_Playable_V10",
         dev: "",
@@ -53,22 +67,22 @@ const BestPA = [
         name: "RC2_V9_Version2_FullborderBacgrkound",
         dev: "",
       },
-         {
+      {
         path: "/PATestDemo/SR_Playable_V2_02_Renamed_Unity_IOS.html",
         name: "SR_Playable_V2_02_Renamed_Unity_IOS",
         dev: "",
       },
-         {
+      {
         path: "/PATestDemo/SR_Playable_V2_01_Renamed_encrypted_ALV.html",
         name: "SR_Playable_V2_01_Renamed_encrypted_ALV",
         dev: "",
       },
-         {
+      {
         path: "/PATestDemo/SR_Playable_V2_01_Renamed_Unity_IOS.html",
         name: "SR_Playable_V2_01_Renamed_Unity_IOS",
         dev: "",
       },
-         {
+      {
         path: "/PATestDemo/SR_Playable_V2_02_Renamed_encrypted_ALV.html",
         name: "SR_Playable_V2_02_Renamed_encrypted_ALV",
         dev: "",
