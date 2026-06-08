60b303cac82e77821aa5ae17f21f2a73f6a578af
HiepDH <hiepdh@hmgames.com>
2026-06-08 17:57:32 +0700

    test RC3

--- src/data.js
+++ src/data.js
@@ -3,49 +3,8 @@ const BestPA = [
     timeline: "List:",
     data: [
       {
-        path: "/PATestDemo/index_unity.html",
-        name: "index_unity",
-        dev: "",
-      },
-      {
-        path: "/PATestDemo/Rc2_InAppData_Clipboard_.html",
-        name: "Rc2_InAppData_Clipboard_",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_01_TypeA_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_01_TypeA_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_02_TypeB_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_02_TypeB_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_03_TypeC_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_03_TypeC_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_04_TypeD_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_04_TypeD_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_05_TypeI_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_05_TypeI_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
-        dev: "",
-      },
-
-       {
-        path: "/PATestDemo/RC3_V46_06_TypeK_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM.html",
-        name: "RC3_V46_06_TypeK_5Chapters_the-mother-he-tried-to-erase QuangBMQuangBM",
+        path: "/PATestDemo/testAdsRC3.html",
+        name: "testAdsRC3",
         dev: "",
       },
     ],
